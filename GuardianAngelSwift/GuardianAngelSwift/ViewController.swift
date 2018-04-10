5cd0fa130183d4756cde0f0ff80203456c75da83
Chris Morris <gspbirel56@gmail.com>
2018-04-10 04:28:37 -0400

    Merged everything to ViewController + got sample entry in table

--- GuardianAngelSwift/GuardianAngelSwift/ViewController.swift
+++ GuardianAngelSwift/GuardianAngelSwift/ViewController.swift
@@ -9,20 +9,20 @@
 import UIKit
 
 class DeadManSwitch {
-    var title: String
-    var time: Date
-    var enabled: Bool
-    var action: Array<DMSAction>
+    public var title: String
+    public var time: String
+    public var enabled: Bool
+    public var action: Array<DMSAction>
     
-    @IBOutlet weak var DMSEntries: DMSEntryListView!
-    init(title Title: String, time Time: Date, enabled Enabled: Bool, action Action: Array<DMSAction>)
+    
+    init(title Title: String, time Time: String, enabled Enabled: Bool, action Action: Array<DMSAction>)
     {
         title = Title
         time = Time
         enabled = Enabled
         action = Action
     }
-    init(title Title: String, time Time: Date, enabled Enabled: Bool)
+    init(title Title: String, time Time: String, enabled Enabled: Bool)
     {
         title = Title
         time = Time
@@ -31,19 +31,48 @@ class DeadManSwitch {
     }
 }
 
-var LoadMessageShown:Bool = false
+
 
 class DMSAction{
     
 }
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var DMSEntries: DMSEntryListView!
+    //below functions are to conform tu UITableView[...] standards
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1 //0 rows in section to start.
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //load a sample entry
+        
+        let cell = DMSEntries.dequeueReusableCell(withIdentifier: "DMSEntryCell") as! DMSEntry
+        cell.titleLabel.text = "Sample!"
+        cell.dateTimeLabel.text = "Sample time"
+        cell.enabledSwitch.isOn = true
+        cell.selectionStyle = .none
+        return cell
+    }
+    
 
+    
+    
+var LoadMessageShown:Bool = false
+    
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
-        loadSampleEntries()
+        //lets this view controller behave as a table view delegate & data source
+        DMSEntries.delegate = self
+        DMSEntries.dataSource = self
+        
+        //loadSampleEntries()
+        super.viewDidLoad()
     }
     
     override func viewDidAppear(_ animated: Bool) { //occurs when the first view appears
@@ -58,12 +87,12 @@ class ViewController: UIViewController {
  */
         
     }
-
+/* deleted when UITableView[...] implemented
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    */
     
     func OKAlert(title: String?, message: String?)
     {
@@ -75,12 +104,54 @@ class ViewController: UIViewController {
     }
     
     private func loadSampleEntries() {
+        //declare the arrays
         var switches = Array<DeadManSwitch>()
-        var switch1:DeadManSwitch = DeadManSwitch(title: "Let the dog out", time: Date(), enabled: true)
-        var switch2:DeadManSwitch = DeadManSwitch(title: "Clean the bird cage", time: Date(), enabled: false)
+        var switchcells = Array<DMSEntry>()
+        //create switch objects
+        var switch1:DeadManSwitch = DeadManSwitch(title: "Let the dog out", time: "now", enabled: true)
+        var switch2:DeadManSwitch = DeadManSwitch(title: "Clean the bird cage", time: "now", enabled: true)
         switches += [switch1, switch2]
+        //add switch objects into usable DMSEntry objects, then -> DMSEntries [view]
+        //switchcells.append(DMSEntry.init(dms: switch1))
+        //switchcells.append(DMSEntry.init(dms: switch2))
         
         
     }
 }
 
+class DMSEntry: UITableViewCell {
+    
+    //MARK: Properties
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateTimeLabel: UILabel!
+    @IBOutlet weak var enabledSwitch: UISwitch!
+    
+    convenience init()
+    {
+        //let aDecoder: NSCoder = NSCoder()
+        self.init(coder: NSCoder())!
+        self.titleLabel.text = "blank"
+        self.dateTimeLabel.text = "blank"
+        self.enabledSwitch.isOn = false
+    }
+    
+    convenience init(title: String, dateTime: String, enabledSwitch: Bool)
+    {
+        self.init()
+        titleLabel.text = title
+        dateTimeLabel.text = dateTime
+        self.enabledSwitch.isOn = enabledSwitch
+        
+    }
+ 
+    convenience init(dms: DeadManSwitch)
+    {
+        self.init(title: dms.title, dateTime: dms.time, enabledSwitch: dms.enabled)
+}
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        //fatalError("init(coder:) has not been implemented")
+    }
+    
+}
